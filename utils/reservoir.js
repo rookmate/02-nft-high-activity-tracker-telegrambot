cef2c11152b6ecef0f32da9e08ff236e6f8c3221
rookmate <14072042+rookmate@users.noreply.github.com>
2024-01-28 22:22:12 +0000

    Fix bug on getFloorData

--- utils/reservoir.js
+++ utils/reservoir.js
@@ -2,14 +2,13 @@ require('dotenv').config();
 const sdk = require('api')('@reservoirprotocol/v3.0#j7ej3alr9o3etb');
 const accessSecrets = require('./secrets');
 
-async function getFloorData(contractSlug) {
+async function getFloorData(collectionSlug) {
   try {
     const key = process.argv.includes('--google')
       ? (await accessSecrets(['RESERVOIR_KEY']))[0]
       : `${process.env.RESERVOIR_KEY}`;
     await sdk.auth(key);
-    const currentBatch = contractAddresses.slice(i, endIndex);
-    const response = await sdk.getCollectionsV7({ slug: contractSlug, accept: '*/*' });
+    const response = await sdk.getCollectionsV7({ slug: collectionSlug, accept: '*/*' });
     const parsedData = response.data.collections.map(collection => ({
       "address": collection.id.toLowerCase(),
       "slug": collection.slug,
